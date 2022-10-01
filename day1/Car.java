cc941c12ea1e498d900696cbb561e0a100817186
TamilarasiRamasamy <114828430+TamilarasiRamasamy@users.noreply.github.com>
2022-10-01 23:38:55 +0530

    Add files via upload
    day1

---
+++ day1/Car.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Car {
+		String colour = "Blue";
+		int vehicleModel = 2022;
+		boolean tested  = true;
+		long price = 1900050l;
+		byte seats= 5;
+		String name = "BMW";
+		float secondsPrice= 23456.789f;
+		double discountedPrice = 345678.89078;
+}
---
+++ day1/HomeAssignment1.java
@@ -0,0 +1,22 @@
+package week1.day1;
+public class HomeAssignment1
+{
+    public static void main(String[] args) {
+	int i =1;int fact;
+	for(fact = 1; fact<=5; fact++) {
+		i = fact*i;	
+	}
+	System.out.println("Factorial of 5 = "+i);	
+    }
+}
+/*Learnt the below
+How to create Class?
+How to declare & assign a variable?
+How to create package?
+For Loop
+Operators
+Datatypes
+Variables
+How to print the output?
+Concatenate Operator
+*/
